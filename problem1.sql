6848a325dc6eb16eed6cc79496ccfc09fa14e555
leestephen0320 <leestephen0320@gmail.com>
2023-02-28 18:11:59 +0000

    modified:   problem1.sql

--- problem1.sql
+++ problem1.sql
@@ -1,5 +1,5 @@
-CREATE TABLE post (
-    id SERIAL PRIMARY KEY,
-    post VARCHAR(255),
-    user_fk int REFERENCES user(id) 
+CREATE TABLE post ( 
+    id SERIAL PRIMARY KEY, 
+    post VARCHAR(255), 
+    user_fk int REFERENCES site_user(id)
 );
